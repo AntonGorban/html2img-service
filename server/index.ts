9f0511fd8d45bb112613d3f4249c2f848a365631
AntonGorban <antogor.work@gmail.com>
2022-04-10 15:32:17 +0300

    add fileUpload middlevare

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,22 @@
 require("dotenv").config();
 import express from "express";
+import fileUpload from "express-fileupload";
+import fse from "fs-extra";
+
+import { paths } from "./src/other/config";
+
+fse.ensureDir(paths.tmp);
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(
+  fileUpload({
+    limits: { fileSize: 20971520 },
+    useTempFiles: true,
+    tempFileDir: paths.tmp,
+  })
+);
 
 (async () => {
   try {
---
+++ server/src/other/config.ts
@@ -0,0 +1,7 @@
+import path from "path";
+
+export const rootPath = path.resolve(__dirname, "..", "..");
+
+export const paths = {
+  tmp: path.resolve(rootPath, "tmp"),
+};

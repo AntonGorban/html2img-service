0026c87f458f242ee5cab1b5334994768a7d127a
AntonGorban <antogor.work@gmail.com>
2022-04-23 19:49:26 +0300

    use App as class

--- server/index.ts
+++ server/index.ts
@@ -1,27 +1,24 @@
 require("dotenv").config();
-import express from "express";
-import fileUpload from "express-fileupload";
 import fse from "fs-extra";
 
+import { App } from "./src/Classes/App";
+import { FileUploadMiddleware } from "./src/Middlewares/FileUploadMiddleware";
 import { paths } from "./src/other/config";
 
 fse.ensureDir(paths.tmp);
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
-const app = express();
-app.use(
-  fileUpload({
-    limits: { fileSize: 20971520 },
-    useTempFiles: true,
-    tempFileDir: paths.tmp,
-  })
+const app = new App(
+  PORT,
+  [
+    new FileUploadMiddleware({
+      limits: { fileSize: 20971520 },
+      useTempFiles: true,
+      tempFileDir: paths.tmp,
+    }),
+  ],
+  []
 );
 
-(async () => {
-  try {
-    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-  } catch (error) {
-    console.error("Server dont started", error);
-  }
-})();
+app.start();

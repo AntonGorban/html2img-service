d3343d72b2608538ccee3683384b39b9d0477e17
AntonGorban <antogor.work@gmail.com>
2022-04-23 19:48:51 +0300

    create FileUploadMiddleware middleware

---
+++ server/src/Middlewares/FileUploadMiddleware.ts
@@ -0,0 +1,22 @@
+import fileUpload from "express-fileupload";
+
+import { Server } from "../../types";
+import { Middleware } from "../Classes/Middleware";
+
+export class FileUploadMiddleware extends Middleware {
+  constructor(private readonly _config: fileUpload.Options) {
+    super();
+  }
+
+  protected _controller: Server.ControllerType<
+    Server.ParamsType,
+    Server.QueryType,
+    Server.BodyType,
+    any
+  > = fileUpload(this._config) as Server.ControllerType<
+    Server.ParamsType,
+    Server.QueryType,
+    Server.BodyType,
+    any
+  >;
+}

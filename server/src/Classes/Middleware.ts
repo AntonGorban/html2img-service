1849cf67f58ab751c8a01fa0523c668d83875e10
AntonGorban <antogor.work@gmail.com>
2022-04-23 18:02:58 +0300

    create Middleware class

---
+++ server/src/Classes/Middleware.ts
@@ -0,0 +1,9 @@
+import { Server } from "../../types";
+import { Controller } from "./Controller";
+
+export abstract class Middleware extends Controller<
+  Server.ParamsType,
+  Server.QueryType,
+  Server.BodyType,
+  any
+> {}
